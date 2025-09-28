46e2614ea5b08eb699dd41cd3d178cacc449de40
mari-mainarte <mariana.mainarte@etec.sp.gov.br>
2025-09-27 22:17:13 -0300

    :sparkles:Feat: Login com conexão local e navegação para página Home funcionando

---
+++ LoginSIME/LoginSIME/Services/Request.cs
@@ -0,0 +1,126 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginSIME.Services
+{
+    public class Request
+    {
+        public async Task<int> PostReturnIntAsync<TResult>(string uri, TResult data, string token)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Authorization
+            = new AuthenticationHeaderValue("Bearer", token);
+
+            var content = new StringContent(JsonConvert.SerializeObject(data));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = await httpClient.PostAsync(uri, content);
+
+            string serialized = await response.Content.ReadAsStringAsync();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                return int.Parse(serialized);
+            else
+                throw new Exception(serialized);
+        }
+
+        public async Task<TResponse> PostLoginAsync<TBody, TResponse>(string uri, TBody data)
+             where TBody : class
+             where TResponse : class
+        {
+            HttpClient httpClient = new HttpClient();
+
+           
+            var content = new StringContent(
+                JsonConvert.SerializeObject(data),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            HttpResponseMessage response = await httpClient.PostAsync(uri, content);
+            string serialized = await response.Content.ReadAsStringAsync();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return JsonConvert.DeserializeObject<TResponse>(serialized);
+            }
+            else
+            {
+                // Se houver erro, jogue a exceção contendo a mensagem de erro do servidor
+                throw new Exception(serialized);
+            }
+        }
+
+        public async Task<TResult> PostAsync<TResult>(string uri, TResult data, string token)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Authorization
+            = new AuthenticationHeaderValue("Bearer", token);
+
+            var content = new StringContent(JsonConvert.SerializeObject(data));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = await httpClient.PostAsync(uri, content);
+            string serialized = await response.Content.ReadAsStringAsync();
+            TResult result = data;
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                result = await Task.Run(() => JsonConvert.DeserializeObject<TResult>(serialized));
+            else
+                throw new Exception(serialized);
+
+            return result;
+        }
+
+        public async Task<int> PutAsync<TResult>(string uri, TResult data, string token)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization
+                = new AuthenticationHeaderValue("Bearer", token);
+
+            var content = new StringContent(JsonConvert.SerializeObject(data));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = await httpClient.PutAsync(uri, content);
+
+            string serialized = await response.Content.ReadAsStringAsync();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                return int.Parse(serialized);
+            else
+                throw new Exception(serialized);
+        }
+
+
+        public async Task<TResult> GetAsync<TResult>(string uri)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
+            string serialized = await response.Content.ReadAsStringAsync();
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                throw new Exception(serialized);
+
+            TResult result = await Task.Run(() => JsonConvert.DeserializeObject<TResult>(serialized));
+            return result;
+        }
+
+        public async Task<int> DeleteAsync(string uri, string token)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            HttpResponseMessage response = await httpClient.DeleteAsync(uri);
+            string serialized = await response.Content.ReadAsStringAsync();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                return int.Parse(serialized);
+            else
+                throw new Exception(serialized);
+        }
+    }
+}
---
+++ LoginSIME/LoginSIME/Services/Usuarios/UsuarioService.cs
@@ -0,0 +1,30 @@
+﻿using LoginSIME.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginSIME.Services.Usuarios
+{
+    public class UsuarioService : Request
+    {
+        private readonly Request _request;
+        private const string _apiUrlBase = "http://localhost:8080/usuarios";
+
+        public UsuarioService()
+        {
+            _request = new Request();
+        }
+
+        public async Task<TokenDTO> PostLoginUsuario(LoginDTO login)
+        {
+            string urlComplementar = "/login";
+
+            TokenDTO tokenDTO = await _request.PostLoginAsync<LoginDTO, TokenDTO>(_apiUrlBase + urlComplementar, login);
+
+            return tokenDTO;
+            
+        }
+    }
+}
