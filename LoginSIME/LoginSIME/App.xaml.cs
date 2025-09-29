3340db8e4361164c12c9e7310564aaaaf1b0ef14
pan-it <paolacapaquira444@gmail.com>
2025-09-28 21:27:13 -0300

    :sparkles:Feat: Adicionar HOME

--- LoginSIME/LoginSIME/App.xaml.cs
+++ LoginSIME/LoginSIME/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace LoginSIME
+﻿using LoginSIME.Views;
+
+namespace LoginSIME
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new HomeView();
             
         }
     }
